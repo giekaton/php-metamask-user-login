10904559e7584961be6eb935559d42ff4eec53e7
sledmonkey <public@bradstewart.com>
2021-10-11 20:35:57 -0600

    working config.

--- root/src/server/config.php
+++ root/src/server/config.php
@@ -1,13 +1,23 @@
 <?php 
 
+$servertype = "mysql"; // mysql, pgsql
 $servername = "192.168.1.148";
+$serverport = 9906; // mysql: 3306, pgsql: 5432
 $username = "root";
 $password = "MYSQL_ROOT_PASSWORD";
 $dbname = "MYSQL_DATABASE";
+$tablename = "users";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, 9906);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+        if ($servertype == "pgsql") {
+                $dsn = "pgsql:host=$servername;port=$serverport;dbname=$dbname;";
+        } elseif ($servertype == "mysql") {
+                $dsn = "mysql:host=$servername;port=$serverport;dbname=$dbname;";
+        } else {
+                die ('DB config error');
+        }
+        $conn = new PDO($dsn, $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}
+catch (PDOException $e) {
+        die($e->getMessage());
 }
